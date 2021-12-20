522510a6582944b3f426c5e0d624c3699b87669a
Emre Ünsal <unsalemre111@gmail.com>
2021-12-20 23:47:02 +0300

    update funtions

--- src/components/TweetCard.js
+++ src/components/TweetCard.js
@@ -2,17 +2,19 @@ import React from "react";
 
 export default function TweetCard({ tweet }) {
   return (
-    <div>
-      <li>{tweet.text}</li>
+    <div style={{ border: "1px solid", marginBottom: "30px" }}>
       <li>
-        {tweet.media.map((media) =>
-          media.type === "jpg" ? (
-            <img width={300} height={300} src={media.url} alt="foto" />
-          ) : (
-            <video width={300} height={300} controls="true" src={media.url} />
-          )
-        )}
+        {tweet.text}
+        <p>{tweet.like}</p>
       </li>
+
+      {tweet.media.map((media) =>
+        media.type === "jpg" ? (
+          <img width={300} height={300} src={media.url} alt="foto" />
+        ) : (
+          <video width={300} height={300} controls="true" src={media.url} />
+        )
+      )}
     </div>
   );
 }
